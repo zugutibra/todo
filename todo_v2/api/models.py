9c512fcd587e618f77f88b5cf1e2cbbfcaaae6d9
Ibrahim Bekturgan <58702396+zugutibra@users.noreply.github.com>
2024-06-26 21:44:30 +0500

    Add files via upload

---
+++ todo_v2/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
